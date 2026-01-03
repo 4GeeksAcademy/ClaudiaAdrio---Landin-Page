0b1cf4fe979c6f30ad5de9fe231a8ba4f0aaac7f
Claudia Adrio <c.adrio@outlook.com>
2026-01-03 18:53:20 +0000

    Full responsive

--- src/js/components/Card.jsx
+++ src/js/components/Card.jsx
@@ -3,20 +3,22 @@ const Card = (props) => {
     const infoCard = props.cardInfo
     return (
 
-        <div className="col-md-3 col-sm-6 mb-3">
-            <div className="card">{infoCard.card}
+        <div className="col-12 col-sm-6 col-md-3 mb-3">
+            <div className="card h-100">{infoCard.card}
                 <div className="ratio ratio-1x1">
-                <img src={infoCard.imgUrl} className="w-100 object-fit-cover"/>
+                    <img src={infoCard.imgUrl}
+                        className="img-fluid object-fit-cover"
+                        alt={infoCard.title} />
                 </div>
-                <div className="card-body">
+                <div className="card-body d-flex flex-column">
                     <h5 className="card-title mb-3">{infoCard.title}</h5>
-                    <p className="card-text" style={{height: "15rem"}}>{infoCard.description}</p>
+                    <p className="card-text">{infoCard.description}</p>
+                </div>
+                <div className="card-footer border-0 bg-white">
                     <a href="#" className="btn btn-outline-danger">Horarios y profesores</a>
                 </div>
-
             </div>
 
-           
         </div>
     )
 }
